ab33f5e0f5ad31242e5921556b55454dce4dc8df
Ryusuke Kubo <kubbo0211@gmail.com>
2019-02-01 17:34:37 +0900

    Add actions in controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
+
   def index
+    @group = Group.find(params[:group_id])
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(create_params)
+  end
+
+  private
+
+  def create_params
+    params.permit(:te)
   end
 end
